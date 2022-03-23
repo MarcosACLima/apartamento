8247ecb993f7dc37eb9e459024e208babb9fd46c
Marcos Lima <48837929+MarcosACLima@users.noreply.github.com>
2022-03-22 21:04:00 -0300

    Implementado EndPoint de atualizar ENUM estado

--- src/main/java/br/com/marcos/apartamento/api/controller/ApartamentoController.java
+++ src/main/java/br/com/marcos/apartamento/api/controller/ApartamentoController.java
@@ -2,7 +2,6 @@ package br.com.marcos.apartamento.api.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,50 +10,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.marcos.apartamento.api.dto.ApartamentoDTO;
+import br.com.marcos.apartamento.api.dto.AtualizarEstadoDTO;
 import br.com.marcos.apartamento.exception.RegraNegocioException;
 import br.com.marcos.apartamento.model.entity.Apartamento;
 import br.com.marcos.apartamento.model.enums.EstadoApartamento;
-import br.com.marcos.apartamento.model.repository.ApartamentoRepository;
 import br.com.marcos.apartamento.service.ApartamentoService;
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequiredArgsConstructor
 //@RequestMapping("/api")
 public class ApartamentoController {
 	
-	@Autowired /* IC/CD ou CDI - Injeção de dependencias */
-	private ApartamentoRepository apartamentoRepository;
-	
-	private ApartamentoService apartamentoService;
-	
-	private ApartamentoController(ApartamentoService apartamentoService) {
-		this.apartamentoService = apartamentoService;
-	}
-	
-	// http://localhost:8080/crud/mostrarnome/digitar string name
-	@RequestMapping(value = "/mostrarnome/{name}", method = RequestMethod.GET)
-	@ResponseStatus(HttpStatus.OK)
-	public String greetingsText(@PathVariable String name) {
-		return "CRUD Spring Boot API " + name + "!";
-	}
-	
-	@RequestMapping(value = "/olamundo/{msg}", method = RequestMethod.GET)
-	@ResponseStatus(HttpStatus.OK)
-	public String retornarOlaMundo(@PathVariable int numero) {
-		
-		Apartamento apartamento = new Apartamento();
-		apartamento.setNumero(numero); // set nome
-		
-		apartamentoRepository.save(apartamento); // gravar no banco de dados
-		
-		return "Ola mundo " + numero;
-	}
+	private final ApartamentoService apartamentoService;
 	
 	/* Primeiro metodo de API */
 	@GetMapping(value = "apartamento")
@@ -106,12 +78,36 @@ public class ApartamentoController {
 			new ResponseEntity("Apartamento deletado com sucesso!", HttpStatus.BAD_REQUEST) );
 	}
 	
+	@PutMapping("produto/{id}/atualizar-estado")
+	public ResponseEntity atualizarEstado( @PathVariable("id") Long id, @RequestBody AtualizarEstadoDTO dto ) {
+		return apartamentoService.obterPorId(id).map( entity -> {
+			EstadoApartamento estadoSelecionado = EstadoApartamento.valueOf(dto.getEstado()); // valueOf buscar a String do ENUM referente ao argumento passado 
+			
+			if(estadoSelecionado == null) {
+				return ResponseEntity.badRequest().body("Não foi possível atualizar o estado do Apartamento, envie um estado válido.");
+			}
+			
+			try {
+				entity.setEstado(estadoSelecionado);
+				apartamentoService.atualizar(entity);
+				return ResponseEntity.ok(entity);
+			} catch (Exception e) {
+				return ResponseEntity.badRequest().body(e.getMessage());
+			}
+			
+		}).orElseGet( () -> 
+			new ResponseEntity("Apartamento não encontrado na base de Dados.", HttpStatus.BAD_REQUEST) );	
+	}
+	
 	private Apartamento converter(ApartamentoDTO apartamentoDTO) {
 		Apartamento apartamento = new Apartamento();
 		apartamento.setId(apartamentoDTO.getId());
 		apartamento.setNumero(apartamentoDTO.getNumero());
-		apartamento.setEstado(EstadoApartamento.valueOf(apartamentoDTO.getEstado())); // valeOf recebe uma String que vai representar uma corresponde da ENUM 
-	
+		
+		if(apartamentoDTO.getEstado() != null) {
+			apartamento.setEstado(EstadoApartamento.valueOf(apartamentoDTO.getEstado())); // valeOf recebe uma String que vai representar uma corresponde da ENUM 
+		}
+		
 		return apartamento;
 	}
 	
---
+++ src/main/java/br/com/marcos/apartamento/api/dto/AtualizarEstadoDTO.java
@@ -0,0 +1,14 @@
+package br.com.marcos.apartamento.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AtualizarEstadoDTO {
+	
+	private String estado;
+
+}
