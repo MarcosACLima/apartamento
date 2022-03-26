a2384ce6a043a0668968d21b75ce6743522e9e4e
Marcos Lima <48837929+MarcosACLima@users.noreply.github.com>
2022-03-26 01:05:20 -0300

    Habilita CORS  'Permiter integração JSON, XML '

--- src/main/java/br/com/marcos/apartamento/ApartamentoApplication.java
+++ src/main/java/br/com/marcos/apartamento/ApartamentoApplication.java
@@ -2,10 +2,26 @@ package br.com.marcos.apartamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
-public class ApartamentoApplication {
+@EnableWebMvc 
+/* 
+ * Habilita CORS
+ * Permiter integração com bean validation, 
+ * suporte geração de RSS, suporte a serialização de objetos para JSON, XML, etc
+ */
+public class ApartamentoApplication implements WebMvcConfigurer {
 
+	@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+		}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApartamentoApplication.class, args);
 	}
